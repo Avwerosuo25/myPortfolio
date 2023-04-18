c64e12d6642015045e54c732cb40a25acde6cbc5
Kelvin Saduwa <85433975+Avwerosuo25@users.noreply.github.com>
2023-04-18 12:32:44 +0100

    Home Section

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Add event listener to navbar toggler button
+document.querySelector('.navbar-toggler').addEventListener('click', function() {
+  // Toggle the 'show' class on the navbar collapse element
+  document.querySelector('.navbar-collapse').classList.toggle('show');
+});
