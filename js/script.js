85aa778c0eb6b3cb52dc277dfa25812d5e9a6f25
Kelvin Saduwa <85433975+Avwerosuo25@users.noreply.github.com>
2023-04-19 16:42:19 +0100

    Worked on the Toggle button

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ document.querySelector('.navbar-toggler').addEventListener('click', function() {
   // Toggle the 'show' class on the navbar collapse element
   document.querySelector('.navbar-collapse').classList.toggle('show');
 });
+
+// Add event listener to document
+document.addEventListener('click', function(event) {
+  // If the user clicked outside of the navbar collapse element, hide it
+  if (!event.target.closest('.navbar-collapse') && document.querySelector('.navbar-collapse').classList.contains('show')) {
+    document.querySelector('.navbar-collapse').classList.remove('show');
+  }
+});
